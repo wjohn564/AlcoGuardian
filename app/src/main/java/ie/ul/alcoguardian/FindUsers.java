bc6f6ecf128cbcd7dfa9e1d38200438d4f9626f6
RuairiG72 <76011537+RuairiG72@users.noreply.github.com>
2023-04-24 05:04:36 +0100

    Finished Friends and FindUsers activity

--- app/src/main/java/ie/ul/alcoguardian/FindUsers.java
+++ app/src/main/java/ie/ul/alcoguardian/FindUsers.java
@@ -1,32 +1,37 @@
 package ie.ul.alcoguardian;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindUsers extends AppCompatActivity {
 
     private EditText searchEmail;
     private Button searchButton;
+    Button backButton;
     private TextView resultTextView;
-
     private FirebaseFirestore db;
-    private CollectionReference collectionReference;
 
-
-    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +39,12 @@ public class FindUsers extends AppCompatActivity {
 
         searchEmail = findViewById(R.id.email_edit_text);
         searchButton = findViewById(R.id.search_button);
-        resultTextView = findViewById(R.id.results_list_view);
+        backButton = findViewById(R.id.back);
+        resultTextView = findViewById(R.id.results_text_view);
 
         db = FirebaseFirestore.getInstance();
-        collectionReference = db.collection("Users");
 
         searchButton.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
                 String email = searchEmail.getText().toString().trim();
@@ -52,28 +56,47 @@ public class FindUsers extends AppCompatActivity {
                 }
             }
         });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Friends.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void searchForUsers(String email) {
-        collectionReference.whereEqualTo("email", email)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            String result = "";
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                result = document.getString("email");
-                            }
-                            if (TextUtils.isEmpty(result)) {
-                                resultTextView.setText("No users found with this email address");
-                            } else {
-                                resultTextView.setText("User found: " + result);
-                            }
-                        } else {
-                            resultTextView.setText("Error searching for users");
-                        }
+        CollectionReference usersRef = db.collection("Users");
+        DocumentReference userDoc = usersRef.document(email);
+
+        userDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String result = document.getString("Email");
+                        resultTextView.setText("User found: " + result);
+
+                        // Add the searched user to the current user's Friends collection
+                        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                        String currentUserEmail = mAuth.getCurrentUser().getEmail();
+                        CollectionReference currentUserFriendsRef = db.collection("Users")
+                                .document(currentUserEmail)
+                                .collection("Friends");
+                        Map<String, String> friendData = new HashMap<>();
+                        friendData.put("Email", result);
+                        currentUserFriendsRef.document(email).set(friendData);
+
+                    } else {
+                        resultTextView.setText("No users found with this email address");
                     }
-                });
+                } else {
+                    resultTextView.setText("Error searching for users");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/ie/ul/alcoguardian/Friends.java
+++ app/src/main/java/ie/ul/alcoguardian/Friends.java
@@ -1,19 +1,24 @@
 package ie.ul.alcoguardian;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -30,14 +35,16 @@ public class Friends extends AppCompatActivity {
     private List<String> friendsList;
     private ListView friendsListView;
 
-    Button search;
+    Button backButton;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
 
-        search = findViewById(R.id.search);
+        searchButton = findViewById(R.id.search);
+        backButton = findViewById(R.id.back);
 
         Intent intent = getIntent();
         auth = FirebaseAuth.getInstance();
@@ -48,11 +55,12 @@ public class Friends extends AppCompatActivity {
         friendsList = new ArrayList<>();
         friendsListView = findViewById(R.id.friendList);
 
-        // Get the "Friends" collection for the current user's email
-        DocumentReference friendsListRef = db.collection("Friends")
-                .document(user.getEmail());
+        // Get the "Friends" subcollection for the current user's email
+        CollectionReference friendsRef = db.collection("Users")
+                .document(user.getEmail())
+                .collection("Friends");
 
-        friendsListRef.collection("Friends").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        friendsRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
@@ -63,10 +71,42 @@ public class Friends extends AppCompatActivity {
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(Friends.this,
                         android.R.layout.simple_list_item_1, friendsList);
                 friendsListView.setAdapter(adapter);
+
+                friendsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String friendEmail = (String) parent.getItemAtPosition(position);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Friends.this);
+                        builder.setTitle("Delete Friend")
+                                .setMessage("Are you sure you want to delete " + friendEmail + " from your friends list?")
+                                .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        friendsRef.document(friendEmail).delete()
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Toast.makeText(Friends.this, "Friend deleted successfully", Toast.LENGTH_SHORT).show();
+                                                        friendsList.remove(friendEmail);
+                                                        adapter.notifyDataSetChanged();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(Friends.this, "Failed to delete friend", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                });
+                                    }
+                                })
+                                .setNegativeButton("Cancel", null)
+                                .create().show();
+                    }
+                });
             }
         });
 
-        search.setOnClickListener(new View.OnClickListener() {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), FindUsers.class);
@@ -74,261 +114,15 @@ public class Friends extends AppCompatActivity {
                 finish();
             }
         });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
 
-
-/*public class Friends extends AppCompatActivity {
-
-    private Button viewButton;
-    private Button deleteFriendButton;
-    String selectedFriendEmail = "";
-    private ListView friendListView;
-    private String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-
-    Map<String, String> friends_list;
-
-    FirebaseFirestore db;
-
-    FirebaseAuth auth;
-
-    FirebaseUser user;
-
-    private DocumentReference doc;
-
-    private boolean friendSelected = false;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friends);
-
-        viewButton = findViewById(R.id.viewProfileButton);
-        deleteFriendButton = findViewById(R.id.deleteFriendButton);
-        friendListView = findViewById(R.id.friendList);
-
-       friendListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                String friendEmail = adapterView.getItemAtPosition(position).toString();
-                friendSelected(friendEmail);
-            }
-        });
-
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
-
-        db = FirebaseFirestore.getInstance();
-
-        doc = db.collection("Friends").document(user.getEmail());
-
-        friends_list = new HashMap<>();
-
-        friends_list.put("Friend", user.getEmail());
-
-        doc.set(friends_list, SetOptions.merge());
-
-        addFriend(user.getEmail(), selectedFriendEmail);
-       viewButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (friendSelected) {
-                    viewFriendProfile(selectedFriendEmail);
-                }
-            }
-        })
-
-        deleteFriendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (friendSelected) {
-                   removeFriend(userEmail, selectedFriendEmail);
-                }
-            }
-       });
-
-    }
-
-   public void getFriends(final OnCompleteListener<QuerySnapshot> listener) {
-        DocumentReference userRef = db.collection("Friends").document(userEmail);
-
-        userRef.get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                            List<String> friends = (List<String>) documentSnapshot.get("Friends");
-                            if (friends != null) {
-                                Query query = db.collection("Friends").document(userEmail).getParent().whereIn("Email", friends);
-                                query.get().addOnCompleteListener(listener);
-                            }
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, "Error getting friends", e);
-                    }
-                });
-    }
-
-
-    public void showFriendList(String userEmail) {
-        db.collection("Friends")
-                .document(userEmail)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                List<String> friendEmails = (List<String>) document.get("friendEmails");
-                                if (friendEmails != null && friendEmails.size() > 0) {
-                                    // Create an ArrayAdapter for the list of friend emails
-                                    ArrayAdapter<String> adapter = new ArrayAdapter<>(Friends.this,
-                                            android.R.layout.simple_list_item_1, friendEmails);
-                                    // Set the adapter on the ListView
-                                   friendListView.setAdapter(adapter);
-                                } else {
-                                    Log.d(TAG, "No friends found");
-                                }
-                            } else {
-                                Log.d(TAG, "No such document");
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting document.", task.getException());
-                        }
-                    }
-                });
-    }
-
-
-    public String friendSelected(String friendRef) {
-        friendSelected = true;
-        selectedFriendEmail = friendRef;
-        viewButton.setEnabled(true);
-        deleteFriendButton.setEnabled(true);
-        return selectedFriendEmail;
-    }
-
-    public void noFriendSelected() {
-        friendSelected = false;
-        String selectedFriendId = null;
-        viewButton.setEnabled(false);
-        deleteFriendButton.setEnabled(false);
-    }
-
-
-    public void addFriend(String userEmail, String friendEmail) {
-        DocumentReference userRef = db.collection("Friends").document(userEmail);
-        DocumentReference friendRef = db.collection("Friends").document(friendEmail);
-
-        userRef.update("Friends" , FieldValue.arrayUnion(friendEmail))
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void v) {
-                        Log.d(TAG, "Friend added successfully");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding friend", e);
-                    }
-                });
-
-        friendRef.update("Friends" , FieldValue.arrayUnion(userEmail))
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void v) {
-                        Log.d(TAG, "Friend added successfully");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding friend", e);
-                    }
-                });
-    }
-
-    public void viewFriendProfile(String friendEmail) {
-        db.collection("Friends")
-                .document(user.getEmail())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                List<String> friends = (List<String>) document.get("friends");
-                                if (friends != null && friends.contains(friendEmail)) {
-                                    db.collection("Users")
-                                            .document("Friends")
-                                            .get()
-                                            .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                    if (task.isSuccessful()) {
-                                                        DocumentSnapshot document = task.getResult();
-                                                        if (document.exists()) {
-                                                            Log.d(TAG, document.getId() + " => " + document.getData());
-                                                            // Do something with the friend's profile data here
-                                                        } else {
-                                                            Log.d(TAG, "No such document");
-                                                        }
-                                                    } else {
-                                                        Log.w(TAG, "Error getting document.", task.getException());
-                                                    }
-                                                }
-                                            });
-                                } else {
-                                    Log.d(TAG, "Friend not found in Friends list");
-                                }
-                            } else {
-                                Log.d(TAG, "No such document");
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting document.", task.getException());
-                        }
-                    }
-                });
-    }
-
-
-    public void removeFriend(String userEmail, String friendEmail) {
-        DocumentReference userRef = db.collection("Friends").document(userEmail);
-        DocumentReference friendRef = db.collection("Friends").document(friendEmail);
-
-        userRef.update("Friends" , FieldValue.arrayRemove(friendEmail))
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void userRef) {
-                        Log.d(TAG, "Friend removed successfully");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error removing friend", e);
-                    }
-                });
-
-        friendRef.update("Friends" , FieldValue.arrayRemove(userEmail))
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void userRef) {
-                        Log.d(TAG, "Friend removed successfully");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error removing friend", e);
-                    }
-                });
-    }
-}*/
